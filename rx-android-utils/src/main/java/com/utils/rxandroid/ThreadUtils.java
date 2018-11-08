90d644e4ef020ec32d0713a3e5ccdea0cab63dd8
Kolyall <kolyall@tut.by>
2018-11-08 16:30:29 +0300

    1.0.7

--- rx-android-utils/src/main/java/com/utils/rxandroid/ThreadUtils.java
+++ rx-android-utils/src/main/java/com/utils/rxandroid/ThreadUtils.java
@@ -1,19 +1,24 @@
 package com.utils.rxandroid;
 
+import android.os.Build;
+import android.os.Looper;
 import android.util.Log;
 
 public class ThreadUtils {
     public static final String TAG = ThreadUtils.class.getSimpleName();
 
+    /**
+     * check that the executed command is not in Main Thread, otherwise will print it in logcat
+     * */
     public static void printThread(String tag) {
-        String threadName = Thread.currentThread().getName();
-        if (threadName.equals("main")) {
-            Log.e(TAG, tag + " is called from " + threadName + " Thread");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                ? Looper.getMainLooper().isCurrentThread()
+                : Thread.currentThread() == Looper.getMainLooper().getThread()) {
+            printThreadName(tag);
         }
     }
 
     public static void printThreadName(String tag) {
-        String threadName = Thread.currentThread().getName();
-        Log.e(TAG, tag + " is called from " + threadName + " Thread");
+        Log.e(TAG, tag + " is called from " + Thread.currentThread().getName() + " Thread");
     }
 }
